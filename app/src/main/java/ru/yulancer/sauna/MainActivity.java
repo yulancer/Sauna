103bc1427fb5ab9edb7631195d233af3e2af6671
yulancer <yulancer@yandex.ru>
2016-03-12 13:42:35 +0300

    обработано изменение ориентации экрана

--- app/src/main/java/ru/yulancer/sauna/MainActivity.java
+++ app/src/main/java/ru/yulancer/sauna/MainActivity.java
@@ -2,6 +2,7 @@ package ru.yulancer.sauna;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -13,11 +14,14 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends FragmentActivity implements SettingsDialog.OnFragmentInteractionListener {
 
+    public static final String SaunaInfoTag = "SaunaInfoTag";
+
     private SaunaSettings mSaunaSettings = new SaunaSettings();
+    private SaunaInfo mSaunaInfo;
 
     private IModbusActor mActor = new Modbus4jActor("192.168.1.77", 502);
     //private IModbusActor mActor = new Modbus4jActor("localhost", 502);
-   // private IModbusActor mActor = new Modbus4jActor("10.0.2.2", 502);
+    // private IModbusActor mActor = new Modbus4jActor("10.0.2.2", 502);
     //private IModbusActor mActor = new J2modActor("10.0.2.2", 502);
 
     @Override
@@ -37,60 +41,15 @@ public class MainActivity extends FragmentActivity implements SettingsDialog.OnF
 
         @Override
         protected void onPostExecute(SaunaInfo saunaInfo) {
-            TextView t0 = (TextView) findViewById(R.id.tempSaunaCurrent);
-            TextView t1 = (TextView) findViewById(R.id.tempBoilerCurrent);
-            TextView t2 = (TextView) findViewById(R.id.tempRoomCurrent);
-            TextView t3 = (TextView) findViewById(R.id.tempWaterpipe);
-            TextView t4 = (TextView) findViewById(R.id.tempOutdoor);
-            TextView t5 = (TextView) findViewById(R.id.tempSaunaSetPoint);
-            TextView t6 = (TextView) findViewById(R.id.tempBoilerSetPoint);
-            TextView t7 = (TextView) findViewById(R.id.tempRoomSetPoint);
-            TextView tvSaunaHeaterStatus = (TextView) findViewById(R.id.tempSaunaHeaterStatus);
-            TextView tvBoilerHeaterStatus = (TextView) findViewById(R.id.tempBoilerHeaterStatus);
-            TextView tvRoomHeaterStatus = (TextView) findViewById(R.id.tempRoomHeaterStatus);
-            TextView tvDoorShower = (TextView) findViewById(R.id.tvDoorShower);
-            TextView tvDoorSauna = (TextView) findViewById(R.id.tvDoorSauna);
-            ToggleButton tbSaunaOn = (ToggleButton) findViewById(R.id.btnSaunaOn);
-            TextView tvException = (TextView) findViewById(R.id.tvException);
-            if (saunaInfo.exception == null) {
-                t0.setText(String.format("%.2f", saunaInfo.SaunaCurrentTemp));
-                t1.setText(String.format("%.2f", saunaInfo.BoilerCurrentTemp));
-                t2.setText(String.format("%.2f", saunaInfo.RoomCurrentTemp));
-                t3.setText(String.format("%.2f", saunaInfo.WaterPipeCurrentTemp));
-                t4.setText(String.format("%.2f", saunaInfo.OutdoorCurrentTemp));
-                t5.setText(String.format("%.2f", saunaInfo.SaunaSetpoint));
-                t6.setText(String.format("%.2f", saunaInfo.BoilerSetpoint));
-                t7.setText(String.format("%.2f", saunaInfo.RoomSetpoint));
-
-                tvSaunaHeaterStatus.setText(saunaInfo.SaunaHeaterOn ? "I" : "O");
-                tvBoilerHeaterStatus.setText(saunaInfo.BoilerHeaterOn ? "I" : "O");
-                tvRoomHeaterStatus.setText(saunaInfo.RoomHeaterOn ? "I" : "O");
-                tvDoorSauna.setText(saunaInfo.DoorSaunaOpen ? "открыта" : "закрыта");
-                tvDoorShower.setText(saunaInfo.DoorShowerOpen ? "открыта" : "закрыта");
-                tbSaunaOn.setChecked(saunaInfo.SaunaOn);
-
-                mSaunaSettings.SaunaSetpoint = saunaInfo.SaunaSetpoint;
-                mSaunaSettings.BoilerSetpoint= saunaInfo.BoilerSetpoint;
-                mSaunaSettings.RoomSetpoint = saunaInfo.RoomSetpoint;
-
-                tvException.setText("");
-            } else {
-                t0.setText("");
-                t1.setText("");
-                t2.setText("");
-                t3.setText("");
-                t4.setText("");
-                t5.setText("");
-                t6.setText("");
-                t7.setText("");
-                tvException.setText(saunaInfo.exception.getLocalizedMessage());
-            }
+            mSaunaInfo = saunaInfo;
+            RefreshSaunaInfo();
 
             ProgressBar p = (ProgressBar) findViewById(R.id.progressBar);
             p.setVisibility(View.GONE);
             Button b = (Button) findViewById(R.id.btnTest);
             b.setEnabled(true);
         }
+
 
         @Override
         protected void onPreExecute() {
@@ -117,6 +76,75 @@ public class MainActivity extends FragmentActivity implements SettingsDialog.OnF
         protected SaunaInfo doInBackground(Void... params) {
             mActor.SaveSettings(mSaunaSettings);
             return super.doInBackground(params);
+        }
+    }
+
+    private void RefreshSaunaInfo() {
+        if (mSaunaInfo != null) {
+            TextView t0 = (TextView) findViewById(R.id.tempSaunaCurrent);
+            TextView t1 = (TextView) findViewById(R.id.tempBoilerCurrent);
+            TextView t2 = (TextView) findViewById(R.id.tempRoomCurrent);
+            TextView t3 = (TextView) findViewById(R.id.tempWaterpipe);
+            TextView t4 = (TextView) findViewById(R.id.tempOutdoor);
+            TextView t5 = (TextView) findViewById(R.id.tempSaunaSetPoint);
+            TextView t6 = (TextView) findViewById(R.id.tempBoilerSetPoint);
+            TextView t7 = (TextView) findViewById(R.id.tempRoomSetPoint);
+            TextView tvSaunaHeaterStatus = (TextView) findViewById(R.id.tempSaunaHeaterStatus);
+            TextView tvBoilerHeaterStatus = (TextView) findViewById(R.id.tempBoilerHeaterStatus);
+            TextView tvRoomHeaterStatus = (TextView) findViewById(R.id.tempRoomHeaterStatus);
+            TextView tvDoorShower = (TextView) findViewById(R.id.tvDoorShower);
+            TextView tvDoorSauna = (TextView) findViewById(R.id.tvDoorSauna);
+            ToggleButton tbSaunaOn = (ToggleButton) findViewById(R.id.btnSaunaOn);
+            TextView tvException = (TextView) findViewById(R.id.tvException);
+            if (mSaunaInfo.exception == null) {
+                t0.setText(String.format("%.2f", mSaunaInfo.SaunaCurrentTemp));
+                t1.setText(String.format("%.2f", mSaunaInfo.BoilerCurrentTemp));
+                t2.setText(String.format("%.2f", mSaunaInfo.RoomCurrentTemp));
+                t3.setText(String.format("%.2f", mSaunaInfo.WaterPipeCurrentTemp));
+                t4.setText(String.format("%.2f", mSaunaInfo.OutdoorCurrentTemp));
+                t5.setText(String.format("%.2f", mSaunaInfo.SaunaSetpoint));
+                t6.setText(String.format("%.2f", mSaunaInfo.BoilerSetpoint));
+                t7.setText(String.format("%.2f", mSaunaInfo.RoomSetpoint));
+
+                tvSaunaHeaterStatus.setText(mSaunaInfo.SaunaHeaterOn ? "I" : "O");
+                tvBoilerHeaterStatus.setText(mSaunaInfo.BoilerHeaterOn ? "I" : "O");
+                tvRoomHeaterStatus.setText(mSaunaInfo.RoomHeaterOn ? "I" : "O");
+                tvDoorSauna.setText(mSaunaInfo.DoorSaunaOpen ? "открыта" : "закрыта");
+                tvDoorShower.setText(mSaunaInfo.DoorShowerOpen ? "открыта" : "закрыта");
+                tbSaunaOn.setChecked(mSaunaInfo.SaunaOn);
+
+                mSaunaSettings.SaunaSetpoint = mSaunaInfo.SaunaSetpoint;
+                mSaunaSettings.BoilerSetpoint = mSaunaInfo.BoilerSetpoint;
+                mSaunaSettings.RoomSetpoint = mSaunaInfo.RoomSetpoint;
+
+                tvException.setText("");
+            } else {
+                t0.setText("");
+                t1.setText("");
+                t2.setText("");
+                t3.setText("");
+                t4.setText("");
+                t5.setText("");
+                t6.setText("");
+                t7.setText("");
+                tvException.setText(mSaunaInfo.exception.getLocalizedMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (mSaunaInfo != null) {
+            outState.putParcelable(SaunaInfoTag, mSaunaInfo);
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Parcelable savedInfo = savedInstanceState.getParcelable(SaunaInfoTag);
+        if (savedInfo != null) {
+            mSaunaInfo = (SaunaInfo) savedInfo;
+            RefreshSaunaInfo();
         }
     }
 
--- app/src/main/java/ru/yulancer/sauna/SaunaInfo.java
+++ app/src/main/java/ru/yulancer/sauna/SaunaInfo.java
@@ -1,9 +1,12 @@
 package ru.yulancer.sauna;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by matveev_yuri on 10.03.2016.
  */
-public class SaunaInfo {
+public class SaunaInfo implements Parcelable {
     public Exception exception;
     public float SaunaCurrentTemp;
     public float BoilerCurrentTemp;
@@ -19,4 +22,60 @@ public class SaunaInfo {
     public boolean DoorSaunaOpen;
     public boolean DoorShowerOpen;
     public boolean SaunaOn;
+
+    public SaunaInfo(){
+
+    }
+
+    protected SaunaInfo(Parcel in) {
+        SaunaCurrentTemp = in.readFloat();
+        BoilerCurrentTemp = in.readFloat();
+        RoomCurrentTemp = in.readFloat();
+        WaterPipeCurrentTemp = in.readFloat();
+        OutdoorCurrentTemp = in.readFloat();
+        SaunaSetpoint = in.readFloat();
+        BoilerSetpoint = in.readFloat();
+        RoomSetpoint = in.readFloat();
+        SaunaHeaterOn = in.readByte() != 0;
+        BoilerHeaterOn = in.readByte() != 0;
+        RoomHeaterOn = in.readByte() != 0;
+        DoorSaunaOpen = in.readByte() != 0;
+        DoorShowerOpen = in.readByte() != 0;
+        SaunaOn = in.readByte() != 0;
+    }
+
+    public static final Creator<SaunaInfo> CREATOR = new Creator<SaunaInfo>() {
+        @Override
+        public SaunaInfo createFromParcel(Parcel in) {
+            return new SaunaInfo(in);
+        }
+
+        @Override
+        public SaunaInfo[] newArray(int size) {
+            return new SaunaInfo[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeFloat(SaunaCurrentTemp);
+        dest.writeFloat(BoilerCurrentTemp);
+        dest.writeFloat(RoomCurrentTemp);
+        dest.writeFloat(WaterPipeCurrentTemp);
+        dest.writeFloat(OutdoorCurrentTemp);
+        dest.writeFloat(SaunaSetpoint);
+        dest.writeFloat(BoilerSetpoint);
+        dest.writeFloat(RoomSetpoint);
+        dest.writeByte((byte) (SaunaHeaterOn ? 1 : 0));
+        dest.writeByte((byte) (BoilerHeaterOn ? 1 : 0));
+        dest.writeByte((byte) (RoomHeaterOn ? 1 : 0));
+        dest.writeByte((byte) (DoorSaunaOpen ? 1 : 0));
+        dest.writeByte((byte) (DoorShowerOpen ? 1 : 0));
+        dest.writeByte((byte) (SaunaOn ? 1 : 0));
+    }
 }
