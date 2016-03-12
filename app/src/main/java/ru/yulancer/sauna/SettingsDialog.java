9c5f89042028215190583e1a0b59782b93dbe21d
yulancer <yulancer@yandex.ru>
2016-03-13 00:25:12 +0300

    ползунки в редакторе температур

--- app/src/main/java/ru/yulancer/sauna/SettingsDialog.java
+++ app/src/main/java/ru/yulancer/sauna/SettingsDialog.java
@@ -8,6 +8,7 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -54,18 +55,89 @@ public class SettingsDialog extends DialogFragment implements DialogInterface.On
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        form = getActivity().getLayoutInflater().inflate(R.layout.fragment_settings, null);
+        form = getActivity().getLayoutInflater().inflate(R.layout.fragment_settings_slider, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         Dialog dialog = builder.setTitle("Заданные температуры").setView(form)
                 .setPositiveButton(android.R.string.ok, this)
                 .setNegativeButton(android.R.string.cancel, null).create();
 
-        EditText etSauna = (EditText) form.findViewById(R.id.etSauna);
-        etSauna.setText(String.format("%.2f", mSaunaSettings.SaunaSetpoint));
-        EditText etBoiler = (EditText) form.findViewById(R.id.etBoiler);
-        etBoiler.setText(String.format("%.2f", mSaunaSettings.BoilerSetpoint));
-        EditText etRoom = (EditText) form.findViewById(R.id.etRoom);
-        etRoom.setText(String.format("%.2f", mSaunaSettings.RoomSetpoint));
+        SeekBar sbSauna = (SeekBar) form.findViewById(R.id.sbSauna);
+        final TextView tvSauna = (TextView) form.findViewById(R.id.tvSetpointSaunaDialog);
+        if (sbSauna != null){
+            sbSauna.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (tvSauna != null)
+                        tvSauna.setText(String.format("%d", progress));
+                    mSaunaSettings.SaunaSetpoint = progress;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+            sbSauna.setProgress((int) mSaunaSettings.SaunaSetpoint);
+        }
+
+        SeekBar sbBoiler = (SeekBar) form.findViewById(R.id.sbBoiler);
+        final TextView tvBoiler = (TextView) form.findViewById(R.id.tvSetpointBoilerDialog);
+        if (sbBoiler != null){
+            sbBoiler.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (tvBoiler != null)
+                        tvBoiler.setText(String.format("%d", progress));
+                    mSaunaSettings.BoilerSetpoint = progress;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+            sbBoiler.setProgress((int) mSaunaSettings.BoilerSetpoint);
+        }
+
+        SeekBar sbRoom = (SeekBar) form.findViewById(R.id.sbRoom);
+        final TextView tvRoom = (TextView) form.findViewById(R.id.tvSetpointRoomDialog);
+        if (sbRoom != null){
+            sbRoom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (tvRoom != null)
+                        tvRoom.setText(String.format("%d", progress));
+                    mSaunaSettings.RoomSetpoint = progress;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+            sbRoom.setProgress((int) mSaunaSettings.RoomSetpoint);
+        }
+//        EditText etSauna = (EditText) form.findViewById(R.id.etSauna);
+//        etSauna.setText(String.format("%.2f", mSaunaSettings.SaunaSetpoint));
+//        EditText etBoiler = (EditText) form.findViewById(R.id.etBoiler);
+//        etBoiler.setText(String.format("%.2f", mSaunaSettings.BoilerSetpoint));
+//        EditText etRoom = (EditText) form.findViewById(R.id.etRoom);
+//        etRoom.setText(String.format("%.2f", mSaunaSettings.RoomSetpoint));
 
         return dialog;
     }
@@ -96,20 +168,21 @@ public class SettingsDialog extends DialogFragment implements DialogInterface.On
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
-        EditText etSauna = (EditText) form.findViewById(R.id.etSauna);
-        EditText etBoiler = (EditText) form.findViewById(R.id.etBoiler);
-        EditText etRoom = (EditText) form.findViewById(R.id.etRoom);
-        try {
-            float tempSauna = ParseLocal(etSauna.getText().toString());
-            float tempBoiler = ParseLocal(etBoiler.getText().toString());
-            float tempRoom = ParseLocal(etRoom.getText().toString());
-            mSaunaSettings = new SaunaSettings(tempSauna, tempBoiler, tempRoom);
-            mListener.onSaveSettings(mSaunaSettings);
-        } catch (Exception e) {
-            TextView tvException = (TextView) getActivity().findViewById(R.id.tvException);
-            tvException.setText(e.toString());
-            e.printStackTrace();
-        }
+        mListener.onSaveSettings(mSaunaSettings);
+//        EditText etSauna = (EditText) form.findViewById(R.id.etSauna);
+//        EditText etBoiler = (EditText) form.findViewById(R.id.etBoiler);
+//        EditText etRoom = (EditText) form.findViewById(R.id.etRoom);
+//        try {
+//            float tempSauna = ParseLocal(etSauna.getText().toString());
+//            float tempBoiler = ParseLocal(etBoiler.getText().toString());
+//            float tempRoom = ParseLocal(etRoom.getText().toString());
+//            mSaunaSettings = new SaunaSettings(tempSauna, tempBoiler, tempRoom);
+//            mListener.onSaveSettings(mSaunaSettings);
+//        } catch (Exception e) {
+//            TextView tvException = (TextView) getActivity().findViewById(R.id.tvException);
+//            tvException.setText(e.toString());
+//            e.printStackTrace();
+//        }
     }
 
     /**
